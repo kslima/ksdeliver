1eecc790131608cc1b3f14c9155a14900f7f559a
Kleuder Lima <kleuder.lima@gmail.com>
2021-09-18 05:50:36 -0300

    Project created

---
+++ backend/src/main/java/com/kslima/ksdeliver/KsdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.kslima.ksdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsdeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kslima/ksdeliver/KsdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kslima.ksdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
